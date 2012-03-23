b9d006fa0a3a5b9fec626c173373aef2eccb51c5
Nuke <flow.energy@gmail.com>
2012-03-23 17:42:53 +0400

    filtration by statuses

--- app/models/journal_observer.rb
+++ app/models/journal_observer.rb
@@ -1,13 +1,18 @@
 JournalObserver.class_eval do
   def after_create(journal)
-    issue = journal.journalized
+    puts 'JournalObserver flag'
     if journal.notify? &&
         (Setting.notified_events.include?('issue_updated') ||
           (Setting.notified_events.include?('issue_note_added') && journal.notes.present?) ||
           (Setting.notified_events.include?('issue_status_updated') && journal.new_status.present?) ||
           (Setting.notified_events.include?('issue_priority_updated') && journal.new_value_for('priority_id').present?)
-        ) && issue.status not in [13]
+        )
       Mailer.deliver_issue_edit(journal)
     end
+  end
+  
+  def after_update(journal)
+    puts journal
+    Mailer.deliver_issue_edit(journal) unless journal.new_value_for('status_id') in [13]
   end
 end

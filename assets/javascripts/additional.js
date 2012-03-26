0f677ec798db1403507962f0148ff7f5d84e030d
Nuke <flow.energy@gmail.com>
2012-03-26 17:01:59 +0400

    final fix for row click

--- assets/javascripts/additional.js
+++ assets/javascripts/additional.js
@@ -1,3 +1,7 @@
-$("tr").filter(".hascontextmenu").click(function() {
-  window.location.href = $(this).find("a").attr("href");
+Event.observe(window, 'load', function() {
+	$$(".hascontextmenu").each(function(item) {
+        item.observe('click', function() {
+                window.location.href = item.select("a").first().readAttribute("href");
+        });
+	});
 });

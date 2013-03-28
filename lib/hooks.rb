745a9c1b2f09e0dac47ec14629f5d102f72a5cdf
Nuke <flow.energy@gmail.com>
2013-03-28 13:16:35 +0400

    hotfixes for query helper

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -1,7 +1,7 @@
   ## Ассигнование заявки к группе
 module RedmineHooks
   class Hooks < Redmine::Hook::ViewListener
-  	render_on :view_issue_statuses_form, :partial => 'issues/notification'
+    render_on :view_issue_statuses_form, :partial => 'issues/notification'
     render_on :view_issues_index_bottom, :partial => 'issues/js_calls'
   end
 
@@ -16,96 +16,96 @@ end
   ## Привязка статуса к проекту
   ## Даты
 module ExtendedProject
-	def self.included(base)
-		base.class_eval do
-			safe_attributes('dates_shown')
-			has_many :issue_statuses
-			def date_activated?
-				self.dates_shown
-			end
-		end
-	end
+  def self.included(base)
+    base.class_eval do
+      safe_attributes('dates_shown')
+      has_many :issue_statuses
+      def date_activated?
+        self.dates_shown
+      end
+    end
+  end
 end
 
 module ExtendedIssue
-	def self.included(base)
-		base.class_eval do
-			def new_statuses_allowed_to(user=User.current, include_default=false)
-				if new_record? && @copied_from
-					[IssueStatus.default_of_project(project), @copied_from.status].compact.uniq.sort
-				else
-					initial_status = nil
-					if new_record?
-						initial_status = IssueStatus.default_of_project(project)
-					elsif status_id_was
-						initial_status = IssueStatus.find_by_id(status_id_was)
-					end
-					initial_status ||= status
+  def self.included(base)
+    base.class_eval do
+      def new_statuses_allowed_to(user=User.current, include_default=false)
+        if new_record? && @copied_from
+          [IssueStatus.default_of_project(project), @copied_from.status].compact.uniq.sort
+        else
+          initial_status = nil
+          if new_record?
+            initial_status = IssueStatus.default_of_project(project)
+          elsif status_id_was
+            initial_status = IssueStatus.find_by_id(status_id_was)
+          end
+          initial_status ||= status
 
-					statuses = initial_status.find_new_statuses_allowed_to(
-								user.admin ? Role.all : user.roles_for_project(project),
-								tracker,
-								author == user,
-								assigned_to_id_changed? ? assigned_to_id_was == user.id : assigned_to_id == user.id
-							   )
-					statuses << initial_status unless statuses.empty?
-					statuses << IssueStatus.default_of_project(project) if include_default
-					statuses = statuses.compact.uniq.sort
-					blocked? ? statuses.reject {|s| s.is_closed?} : statuses
-				end
-			end
-		end
-	end
+          statuses = initial_status.find_new_statuses_allowed_to(
+                user.admin ? Role.all : user.roles_for_project(project),
+                tracker,
+                author == user,
+                assigned_to_id_changed? ? assigned_to_id_was == user.id : assigned_to_id == user.id
+                 )
+          statuses << initial_status unless statuses.empty?
+          statuses << IssueStatus.default_of_project(project) if include_default
+          statuses = statuses.compact.uniq.sort
+          blocked? ? statuses.reject {|s| s.is_closed?} : statuses
+        end
+      end
+    end
+  end
 end
 
   ## Статус привязан к проекту
 module ExtendedIssueStatus
-	def self.included(base)
-  		base.class_eval do
-  			belongs_to :project
+  def self.included(base)
+      base.class_eval do
+        belongs_to :project
 
-  			def update_default
-  				IssueStatus.update_all({:is_default => false}, ['id <> ? AND project_id = ?', id, self.project_id]) if self.is_default?
-  			end
+        def update_default
+          IssueStatus.update_all({:is_default => false}, ['id <> ? AND project_id = ?', id, self.project_id]) if self.is_default?
+        end
 
-  			def self.default_of_project(project)
-  				includes(:project).where(:is_default => true, :project_id => project).first
-  			end
-  		end
-  	end
+        def self.default_of_project(project)
+          includes(:project).where(:is_default => true, :project_id => project).first
+        end
+      end
+    end
 end
 
 module ExtendedIssuesController
-	def self.included(base)
-		base.class_eval do
-			helper :extended_issues
-		end
-	end
+  def self.included(base)
+    base.class_eval do
+      helper :extended_issues
+    end
+  end
 end
 
 module ExtendedJournalObserver
-	def self.included(base)
-		base.class_eval do
-			def after_create_with_checks journal
-				send = false
-				if journal.notify? &&
-				(Setting.notified_events.include?('issue_updated') ||
-				(Setting.notified_events.include?('issue_note_added') && journal.notes.present?) ||
-				(Setting.notified_events.include?('issue_status_updated') && journal.new_status.present?) ||
-				(Setting.notified_events.include?('issue_priority_updated') && journal.new_value_for('priority_id').present?)
-				)
-					send = true
-				end
+  def self.included(base)
+    base.class_eval do
+      def after_create_with_checks journal
+        send = false
+        if journal.notify? &&
+        (Setting.notified_events.include?('issue_updated') ||
+        (Setting.notified_events.include?('issue_note_added') && journal.notes.present?) ||
+        (Setting.notified_events.include?('issue_status_updated') && journal.new_status.present?) ||
+        (Setting.notified_events.include?('issue_priority_updated') && journal.new_value_for('priority_id').present?)
+        )
+          send = true
+        end
 
-				if (journal.new_status.present? && IssueStatus.find(:all, :conditions => { :no_notification => true }).map(&:id).include?(journal.new_value_for('status_id')))
-					send = false
-				end
+        if (journal.new_status.present? && IssueStatus.find(:all, :conditions => { :no_notification => true }).map(&:id).include?(journal.new_value_for('status_id')))
+          send = false
+        end
 
-				Mailer.issue_edit(journal).deliver if send
-			end
-			alias_method_chain :after_create, :checks
-		end
-	end
+        Mailer.issue_edit(journal).deliver if send
+      end
+      alias_method_chain :after_create, :checks
+    end
+  end
 end
 
 module IssuesHelper
@@ -124,15 +124,15 @@ module IssuesHelper
 end
 
 module ExtendedIssueStatusesController
-	def self.included(base)
-	  base.class_eval do
-  		def edit_new
-  			@issue_status = IssueStatus.find(params[:id])
-  			@projects = Project.active
-  		end
-		  alias_method :edit, :edit_new
-	  end
-	end
+  def self.included(base)
+    base.class_eval do
+      def edit_new
+        @issue_status = IssueStatus.find(params[:id])
+        @projects = Project.active
+      end
+      alias_method :edit, :edit_new
+    end
+  end
 end
 
 module ExtendedProjectsController
@@ -180,11 +180,9 @@ module ExtendedQueriesHelper
           @query.project = @project
           build_query_from_params
         else
-          logger.debug "1__________"
-          @query = Query.includes(:project).where(:default => true, :project_id => @project.id).first
-          #@query = Query.find_by_id(session[:query][:id]) if session[:query][:id]
-          #@query ||= Query.new(:name => "_", :filters => session[:query][:filters], :group_by => session[:query][:group_by], :column_names => session[:query][:column_names])
-          #@query.project = @project
+          @query = Query.includes(:project).where(:default => true, :project_id => @project.id).first 
+          (@query ||= Query.new(:name => "_") and @query.project = @project) if @query.nil?
+          sort_clear
         end
       end
     end

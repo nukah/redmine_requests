59f4199670cf233caabf94323ccd3fb0b17b4735
Nuke <flow.energy@gmail.com>
2013-05-21 12:59:49 +0400

    another fix

--- assets/javascripts/refresh_issues.js
+++ assets/javascripts/refresh_issues.js
@@ -1,10 +1,9 @@
 var loc = window.location.href;
 function refresh() {
   $.get(loc, function(data) {
-    $("div.autoscroll").html(data);
+    $("div#content").html(data);
   });
 }
 $(document).ready(function() {
   setInterval(function() { refresh(); }, 10000);
-  console.log("refresh run");
 });
